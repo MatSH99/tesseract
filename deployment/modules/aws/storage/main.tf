dd6288b42d0a408c977487a75634412cbe73cd94
MatSH99 <51320263+MatSH99@users.noreply.github.com>
2026-04-09 09:38:01 +0200

    Create Secrets Manager resources for DB password
    Add resources for managing database credentials in AWS Secrets Manager.

--- deployment/modules/aws/storage/main.tf
+++ deployment/modules/aws/storage/main.tf
@@ -48,6 +48,26 @@ resource "aws_db_subnet_group" "db" {
   subnet_ids = data.aws_subnets.all.ids
 }
 
+resource "random_password" "master" {
+  length           = 16
+  special          = true
+  override_special = "!#$%&*()-_=+[]" # Evitiamo caratteri che possono dare noie in bash
+}
+
+resource "aws_secretsmanager_secret" "db_pass" {
+  name = "${var.base_name}-rds-fixed-password"
+}
+
+resource "aws_secretsmanager_secret_version" "db_pass" {
+  secret_id     = aws_secretsmanager_secret.db_pass.id
+  secret_string = jsonencode({
+    username = aws_rds_cluster.log_rds_cluster.master_username
+    password = random_password.master.result
+    host     = aws_rds_cluster.log_rds_cluster.endpoint
+    port     = 3306
+  })
+}
+
 resource "aws_rds_cluster" "log_rds_cluster" {
   cluster_identifier          = "${var.base_name}-cluster"
   engine                      = "aurora-mysql"
